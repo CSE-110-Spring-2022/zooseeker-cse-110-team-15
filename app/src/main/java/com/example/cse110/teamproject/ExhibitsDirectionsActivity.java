f84c7caf609e741f3a1635b7b8ffafa6783a4bf6
sudolucy <77044790+sudolucy@users.noreply.github.com>
2022-05-07 23:19:23 -0700

    update ExhibitsDirectionsActivity (ITS WORKING)

--- app/src/main/java/com/example/cse110/teamproject/ExhibitsDirectionsActivity.java
+++ app/src/main/java/com/example/cse110/teamproject/ExhibitsDirectionsActivity.java
@@ -2,7 +2,9 @@ package com.example.cse110.teamproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,6 +14,7 @@ import org.jgrapht.GraphPath;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class ExhibitsDirectionsActivity extends AppCompatActivity {
     Button prevButton;
@@ -21,6 +24,7 @@ public class ExhibitsDirectionsActivity extends AppCompatActivity {
     TextView destLocation;
     TextView prevButtonLabel;
     TextView nextButtonLabel;
+    TextView directionSteps;
 
     int directionOrder;
     List<GraphPath<String, IdentifiedWeightedEdge>> pathList;
@@ -29,8 +33,11 @@ public class ExhibitsDirectionsActivity extends AppCompatActivity {
     Graph<String, IdentifiedWeightedEdge> zooGraph;
     Map<String, ZooData.EdgeInfo> eInfo;
     String directions;
-    String directionFormat = "%d. Walk %.0f meters along %s from '%s' to '%s'.\n";
+    final String DIR_FORMAT = "%d. Walk %.0f feet along %s from '%s' to '%s'.\n\n";
+    final String LABEL_FORMAT = "(%s, %.0f ft)";
+    final String DIST_FORMAT = "%.0f ft";
     double totalDistance = 0;
+    final String EMPTY_STRING = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,78 +51,110 @@ public class ExhibitsDirectionsActivity extends AppCompatActivity {
         destLocation = findViewById(R.id.dest_loc);
         prevButtonLabel = findViewById(R.id.prev_button_label);
         nextButtonLabel = findViewById(R.id.next_button_label);
+        directionSteps = findViewById(R.id.direction_steps);
+
+        directionOrder = 0;
+        directions = EMPTY_STRING; // initialize EMPTY_STRING constant
 
         // find path
         zooGraph = ZooData.loadZooGraphJSON(this,"sample_zoo_graph.json");
         pathList = PathFinder.findPath(this);
 
         // get first path in the list
-        directionOrder = 0;
         currentPath = pathList.get(directionOrder);
 
-        // 2. Load the information about our nodes and edges...
+        // disable prev button when on first page
+        prevButton.setEnabled(directionOrder != 0);
+        nextButton.setEnabled(directionOrder != pathList.size() - 1);
+
+        // Load the information about edges
         eInfo = ZooData.loadEdgeInfoJSON(this,"sample_edge_info.json");
 
         exhibitListItemDao = ExhibitDatabase.getSingleton(this).exhibitListItemDao();
+
+        displayDirection();
+        displayDestinationInfo();
+        updateButtonAndLabel();
     }
 
     public void onPreviousIconClicked(View view) {
-        prevButton.setOnClickListener(v -> {
             currentPath = pathList.get(--directionOrder);
 
             displayDirection();
             displayDestinationInfo();
-
-
-            prevButton.setEnabled(directionOrder != 0);
-            if (directionOrder == 0) {
-                prevButtonLabel.setText("");
-            }
-        });
+            updateButtonAndLabel();
     }
 
     public void onNextIconClicked(View view) {
-        prevButton.setOnClickListener(v -> {
             currentPath = pathList.get(++directionOrder);
 
             displayDirection();
             displayDestinationInfo();
-
-            nextButton.setEnabled(directionOrder != pathList.size() - 1);
-            if (directionOrder == pathList.size() - 1) {
-                nextButtonLabel.setText("");
-            }
-        });
+            updateButtonAndLabel();
     }
 
+    @SuppressLint("DefaultLocale")
     public void displayDirection() {
         int i = 1;
+        List<String> vertexList = currentPath.getVertexList();
         for (IdentifiedWeightedEdge e : currentPath.getEdgeList()) {
-            ExhibitNodeItem source = exhibitListItemDao.getExhibitByNodeId(zooGraph.getEdgeSource(e));
-            ExhibitNodeItem target = exhibitListItemDao.getExhibitByNodeId(zooGraph.getEdgeTarget(e));
-            directions += String.format(directionFormat,
+            ExhibitNodeItem source = exhibitListItemDao.getExhibitByNodeId(vertexList.get(i-1));
+            ExhibitNodeItem target = exhibitListItemDao.getExhibitByNodeId(vertexList.get(i));
+            directions += String.format(DIR_FORMAT,
                     i,
                     zooGraph.getEdgeWeight(e),
-                    eInfo.get(e.getId()).street,
+                    Objects.requireNonNull(eInfo.get(e.getId())).street,
                     source.name,
                     target.name);
             i++;
         }
-//        directionView.setText(directions);
-        directions = "";
+        directionSteps.setText(directions);
+        directions = EMPTY_STRING;
     }
 
     public void displayDestinationInfo() {
-        List<IdentifiedWeightedEdge> pathEdgeList = currentPath.getEdgeList();
-        IdentifiedWeightedEdge lastEdge = pathEdgeList.get(pathEdgeList.size()-1);
-        ExhibitNodeItem destinationNode = exhibitListItemDao.getExhibitByNodeId(zooGraph.getEdgeTarget(lastEdge));
-        String street = eInfo.get(lastEdge.getId()).street;
+        List<IdentifiedWeightedEdge> edgeList = currentPath.getEdgeList();
+        String destId = currentPath.getEndVertex();
+        ExhibitNodeItem destinationNode = exhibitListItemDao.getExhibitByNodeId(destId);
+
 
         destName.setText(destinationNode.name);
-        destDistance.setText(Double.toString(currentPath.getWeight()));
+
+        @SuppressLint("DefaultLocale") String distance = String.format(DIST_FORMAT, currentPath.getWeight());
+        destDistance.setText(distance);
+
+
+        String street = Objects.requireNonNull(eInfo.get(edgeList.get(edgeList.size()-1).getId())).street;
         destLocation.setText(street);
 
-        prevButtonLabel.setText(destinationNode.name + "," + Double.toString(currentPath.getWeight()));
-        totalDistance = 0;
+        @SuppressLint("DefaultLocale") String label = String.format(LABEL_FORMAT, destinationNode.name, currentPath.getWeight());
+        prevButtonLabel.setText(label);
+
+        if (directionOrder == pathList.size()-1) {
+            nextButtonLabel.setText(EMPTY_STRING);
+        }
+        else {
+            GraphPath<String, IdentifiedWeightedEdge> nextPath = pathList.get(directionOrder + 1);
+            String nextDestId = nextPath.getEndVertex();
+            ExhibitNodeItem nextDestNode = exhibitListItemDao.getExhibitByNodeId(nextDestId);
+            label = String.format(LABEL_FORMAT, nextDestNode.name, nextPath.getWeight());
+            nextButtonLabel.setText(label);
+        }
+        totalDistance = 0.0;
     }
+
+    public void updateButtonAndLabel() {
+        prevButton.setEnabled(directionOrder != 0);
+        nextButton.setEnabled(directionOrder != pathList.size() - 1);
+
+        if (directionOrder == 0) {
+            prevButtonLabel.setText(EMPTY_STRING);
+        }
+
+        if (directionOrder == pathList.size() - 1) {
+            nextButtonLabel.setText(EMPTY_STRING);
+        }
+    }
+
+
 }
