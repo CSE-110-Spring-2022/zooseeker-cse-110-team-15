97756f8b0835a4981742af4e10b7005bc8196d04
grnnja <grnnja@gmail.com>
2022-05-28 21:52:46 -0700

    add Direction to PathInfo
    direction represents the direction the path is (default forward)

--- app/src/main/java/com/example/cse110/teamproject/path/PathInfo.java
+++ app/src/main/java/com/example/cse110/teamproject/path/PathInfo.java
@@ -5,12 +5,28 @@ import com.example.cse110.teamproject.IdentifiedWeightedEdge;
 import org.jgrapht.GraphPath;
 
 public class PathInfo {
+    public enum Direction {
+        FORWARDS, REVERSE
+    }
     protected GraphPath<String, IdentifiedWeightedEdge> path;
+    protected Direction direction = Direction.FORWARDS;
     PathInfo(GraphPath<String, IdentifiedWeightedEdge> path) {
         this.path = path;
     }
 
     public GraphPath<String, IdentifiedWeightedEdge> getPath() {
         return this.path;
+    }
+
+    protected void setPath(GraphPath<String, IdentifiedWeightedEdge> path) {
+        this.path = path;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
     }
 }
